3767192096ff0207f79352f0f90e16c01de7426f
Cody Persinger <codypersinger@gmail.com>
2020-06-09 12:55:05 -0500

    Fix cylinder axis orientation, update deps, bump version to 0.10.0

--- dist/cannon-es.js
+++ dist/cannon-es.js
@@ -7764,10 +7764,10 @@ class Cylinder extends ConvexPolyhedron {
     const cos = Math.cos;
     const sin = Math.sin; // First bottom point
 
-    vertices.push(new Vec3(radiusBottom * cos(0), radiusBottom * sin(0), -height * 0.5));
+    vertices.push(new Vec3(radiusBottom * cos(0), -height * 0.5, -radiusBottom * sin(0)));
     bottomface.push(0); // First top point
 
-    vertices.push(new Vec3(radiusTop * cos(0), radiusTop * sin(0), height * 0.5));
+    vertices.push(new Vec3(radiusTop * cos(0), height * 0.5, -radiusTop * sin(0)));
     topface.push(1);
 
     for (let i = 0; i < N; i++) {
@@ -7776,10 +7776,10 @@ class Cylinder extends ConvexPolyhedron {
 
       if (i < N - 1) {
         // Bottom
-        vertices.push(new Vec3(radiusBottom * cos(theta), radiusBottom * sin(theta), -height * 0.5));
+        vertices.push(new Vec3(radiusBottom * cos(theta), -height * 0.5, -radiusBottom * sin(theta)));
         bottomface.push(2 * i + 2); // Top
 
-        vertices.push(new Vec3(radiusTop * cos(theta), radiusTop * sin(theta), height * 0.5));
+        vertices.push(new Vec3(radiusTop * cos(theta), height * 0.5, -radiusTop * sin(theta)));
         topface.push(2 * i + 3); // Face
 
         faces.push([2 * i + 2, 2 * i + 3, 2 * i + 1, 2 * i]);
@@ -7789,12 +7789,12 @@ class Cylinder extends ConvexPolyhedron {
 
 
       if (N % 2 === 1 || i < N / 2) {
-        axes.push(new Vec3(cos(thetaN), sin(thetaN), 0));
+        axes.push(new Vec3(cos(thetaN), 0, -sin(thetaN)));
       }
     }
 
     faces.push(topface);
-    axes.push(new Vec3(0, 0, 1)); // Reorder bottom face
+    axes.push(new Vec3(0, 1, 0)); // Reorder bottom face
 
     const temp = [];
 
