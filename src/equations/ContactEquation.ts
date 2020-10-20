da60b5aedbfe5d6a55827bb7312681111c26befa
Patrick Canfield <patrick.canfield@rallyhealth.com>
2020-10-20 08:46:04 -0700

    Revert to not *doc comments for now

--- src/equations/ContactEquation.ts
+++ src/equations/ContactEquation.ts
@@ -13,20 +13,9 @@ import type { Body } from '../objects/Body'
  */
 export class ContactEquation extends Equation {
   restitution: number // "bounciness": u1 = -e*u0
-  /**
-   * World-oriented vector that goes from the center of bi to the contact
-   * point.
-   */
-  ri: Vec3
-  /**
-   * World-oriented vector that starts in body j position and goes to the
-   * contact point.
-   */
-  rj: Vec3
-  /**
-   * Contact normal, pointing out of body i.
-   */
-  ni: Vec3
+  ri: Vec3 // World-oriented vector that goes from the center of bi to the contact point.
+  rj: Vec3 // World-oriented vector that starts in body j position and goes to the contact point.
+  ni: Vec3 // Contact normal, pointing out of body i.
 
   constructor(bodyA: Body, bodyB: Body, maxForce = 1e6) {
     super(bodyA, bodyB, 0, maxForce)
